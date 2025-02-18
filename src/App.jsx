d6109c01c6d56fb698ad239b9f3ce4cec5426991
Tahabutt1 <70131118@student.uol.edu.pk>
2025-02-18 16:05:37 +0500

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Login from "./Pages/LoginPage";
 import RegisterPage from "./Pages/Register";
 import Home from "./Pages/Home";
 import KycRegister from "./Pages/kycRegister";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+
+function Layout() {
+  const location = useLocation();
+  const hideNavbarAndFooter = ["/", "/register"].includes(location.pathname);
+
+  return (
+    <>
+      {!hideNavbarAndFooter && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/kycRegister" element={<KycRegister />} />
+      </Routes>
+      {!hideNavbarAndFooter && <Footer />}
+    </>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <Routes>
-        {/* Login page is the initial page */}
-        <Route path="/" element={<Login />} />
-        {/* Sign Up / Register page */}
-        <Route path="/register" element={<RegisterPage />} />
-        {/* Home page after successful login */}
-        <Route path="/home" element={<Home />} />
-        {/* KYC Registration page */}
-        <Route path="/kycRegister" element={<KycRegister />} />
-      </Routes>
+      <Layout />
     </Router>
   );
 }
